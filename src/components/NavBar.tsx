fb0a44c3c8a3dd1defc9af169756a6ca9927393c
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-02-06 22:25:19 -0300

    style: style added at navBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,45 @@
-import { HStack, Link } from "@chakra-ui/react"
+import { HStack, Link, Box } from "@chakra-ui/react"
 
 export default function NavBar() {
+  const hrefs = [
+    {
+      title: "Sobre",
+      id: "#about"
+    },
+    {
+      title: "Projetos",
+      id: "#projects"
+    },
+    {
+      title: "Contatos",
+      id: "#contacts"
+    }
+  ]
   return (
-    <HStack>
-      <Link href="#about">Sobre</Link>
-      <Link href="#projects">Projetos</Link>
-      <Link href="#contacts">Contatos</Link>
+    <HStack 
+      w="100%"
+      h="100px"
+      justifyContent="center"
+    >
+
+      <HStack
+        w="500px"
+        justifyContent="space-around"
+      >
+        {
+          hrefs.map((href)=>(
+            <Link 
+              href={href.id}
+              fontWeight="700"
+              fontSize="20px"
+              textDecor="underline"
+              _hover={{cursor: "pointer", fontSize: "24px"}}
+            >
+              {href.title}
+            </Link>
+          ))
+        }
+      </HStack> 
     </HStack>
   )
 }
