42ab21bb1a884ee8a79956a87852ec6df8405913
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-02-09 13:27:13 -0300

    refactor: description altered

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,7 @@
 import { Box, Image, HStack, Link, Text, VStack, Heading, Button } from "@chakra-ui/react";
 import { MdOutlineLink } from "react-icons/md";
 import { FaGithub } from "react-icons/fa";
+import { IoCheckmarkCircleOutline } from "react-icons/io5";
 
 import { projects } from "../data";
 
@@ -10,7 +11,7 @@ export default function Projects() {
     <VStack
       id="projects"
       bgColor="#1D1D1F"
-      padding={{base: "0",md: "3rem"}}
+      padding={{ base: "0", md: "3rem" }}
       scrollMarginTop="5rem"
     >
       <Heading as="h2" fontSize={{ base: "3rem", md: "4rem" }} textAlign="center">
@@ -26,32 +27,40 @@ export default function Projects() {
               boxShadow="0 0 1rem black"
               w="100%"
               maxW="30rem"
-              h={{base: "auto", md: "38rem"}}
+              h={{ base: "auto", md: "40rem" }}
               position="relative"
               margin="1rem"
               padding="1rem"
               borderRadius="1rem"
             >
               <VStack h="14rem" w="100%" padding="3rem" bgColor="#a8a8a8">
-                <Image src={project.img} h="100%" borderRadius="1rem"/>
+                <Image src={project.img} h="100%" borderRadius="1rem" />
               </VStack>
 
               <Heading as="h3" fontSize="1.5rem" fontWeight="700" padding="1rem" >
                 {project.name}
               </Heading>
 
-              <Text 
-                padding={{base: "0.5rem", md:"0 1rem 0 1rem"}} 
-                fontSize={{base: "0.75rem", md: "1.125rem" }}
-                textAlign="justify"
-                overflowY="auto"
-                height="7rem"
-                marginBottom="1rem"
-              >
-                {project.description}
-              </Text>
 
-              <HStack w="100%" padding={{base: "0.5rem", md:"0 1rem 0 1rem"}} flexWrap="wrap">
+              <HStack as="ul" height="9rem" flexWrap="wrap" justifyContent="space-between" marginBottom="1rem" overflowY="auto">
+                {
+                  project.functions.map((funct) => (
+                    <Text
+                      as="li"
+                      fontSize={{ base: "0.75rem", md: "1.125rem" }}
+                      textAlign="left"
+                      w="13rem"
+                      display="flex"
+                      color="#d6d6d6"
+                    >
+                      <Box marginRight="0.25rem"><IoCheckmarkCircleOutline /></Box>
+                      {funct}
+                    </Text>
+                  ))
+                }
+              </HStack>
+
+              <HStack w="100%" padding={{ base: "0.5rem", md: "0 1rem 0 1rem" }} flexWrap="wrap">
                 {project.technologies.map((technology) => (
                   <VStack
                     key={technology}
@@ -62,7 +71,7 @@ export default function Projects() {
                     h="2rem"
                     padding="0 1rem 0 1rem"
                   >
-                    <Text fontSize={{base: "0.75rem", md: "1rem"}} color="black">{technology}</Text>
+                    <Text fontSize={{ base: "0.75rem", md: "1rem" }} color="black">{technology}</Text>
                   </VStack>
                 ))}
               </HStack>
@@ -71,34 +80,34 @@ export default function Projects() {
                 fontSize="1.25rem"
                 w="20rem"
                 justifyContent="space-around"
-                position={{base: "relative", md: "absolute"}}
-                bottom={{ md: "1.5rem"}}
+                position={{ base: "relative", md: "absolute" }}
+                bottom={{ md: "1.5rem" }}
                 h="3rem"
-                marginBottom={{base: "1rem", md: "0"}}
+                marginBottom={{ base: "1rem", md: "0" }}
                 color="#000000"
               >
                 {
                   project.url &&
-                  <Link 
+                  <Link
                     alignItems="center"
                     justifyContent="center"
-                    href={project.url} 
+                    href={project.url}
                     target="_blank"
                     w="9rem"
                   >
-                    <Button color="black" backgroundColor="white" w="8rem" h="2.5rem" _hover={{ width: "9rem",h: "3rem"}}>
+                    <Button color="black" backgroundColor="white" w="8rem" h="2.5rem" _hover={{ width: "9rem", h: "3rem" }}>
                       <MdOutlineLink /> Ver site
                     </Button>
                   </Link>
                 }
-                <Link 
+                <Link
                   alignItems="center"
                   justifyContent="center"
-                  href={project.github} 
+                  href={project.github}
                   target="_blank"
                   w="9rem"
                 >
-                  <Button color="black" backgroundColor="white" w="8rem" h="2.5rem" _hover={{ width: "9rem",   h: "3rem" }}>
+                  <Button color="black" backgroundColor="white" w="8rem" h="2.5rem" _hover={{ width: "9rem", h: "3rem" }}>
                     <FaGithub /> Ver projeto
                   </Button>
                 </Link>
--- src/data.tsx
+++ src/data.tsx
@@ -4,15 +4,108 @@ import { FaHome } from "react-icons/fa";
 import { FaRegFolder } from "react-icons/fa";
 import { MdOutlinePhone } from "react-icons/md";
 
+// export const projects = [
+//   {
+//     name: "Track-it",
+//     img: './images/projectsImages/track-it.svg',
+//     url: "https://track-it-seven-amber.vercel.app/",
+//     description: `
+//     O Sistema de Gerenciamento de Rotina é uma ferramenta desenvolvida para auxiliar no controle e validação de tarefas diárias, garantindo maior organização, produtividade e eficiência.
+//     Com essa solução, os usuários podem cadastrar, monitorar e validar suas atividades, garantindo que nenhuma tarefa essencial seja esquecida. O sistema permite a criação de fluxos de trabalho personalizados, estabelecendo regras e verificações para garantir que cada passo da rotina seja cumprido corretamente.
+//     `,
+//     technologies: ["react", "styled-components"],
+//     github: "https://github.com/FelipeMedeiros99/track-it"
+//   },
+//   {
+//     name: "Agenda escolar",
+//     img: './images/projectsImages/school-spreadsheet.png',
+//     url: "http://school-spreadsheet-front.s3-website-sa-east-1.amazonaws.com/sign-in",
+//     description: `
+//       O Sistema de Gerenciamento de Alunos é uma aplicação eficiente para armazenar e organizar informações acadêmicas, permitindo um controle preciso dos estudantes.
+//       Com uma interface intuitiva, o sistema permite cadastrar, visualizar, editar e excluir alunos, garantindo que os dados estejam sempre atualizados. Todas as informações, como nome completo, idade e turma, são apresentadas de forma clara em uma tabela dinâmica, facilitando a gestão e a consulta de registros.
+//     `,
+//     technologies: ["TypeScript", "React", "chakra UI", "AWS"],
+//     github: "https://github.com/FelipeMedeiros99/school-spreadsheet-front"
+//   },
+//   {
+//     name: "Agenda escolar API",
+//     img: "./images/projectsImages/school-spreadsheet.png",
+//     url: "",
+//     description: `
+//     Esta API foi desenvolvida para gerenciar o cadastro de estudantes e usuários, permitindo operações essenciais como criação, edição e exclusão de alunos, garantindo controle total aos usuários autenticados.
+//     A segurança é uma prioridade no projeto, com a implementação de criptografia de senhas, autenticação via tokens e validação de dados utilizando Joi, assegurando a proteção e a consistência das informações.
+//     Os dados são armazenados e gerenciados de forma eficiente no banco de dados PostgreSQL, garantindo escalabilidade e confiabilidade.
+//     `,
+//     technologies: ["TypeScript", "Express", "Bcrypt", "Joi", "PostgreSQL", "Prisma"],
+//     github: "https://github.com/FelipeMedeiros99/school-spreadsheet-backend/tree/main"
+//   },
+//   {
+//     name: "Gerenciamento de Cursos",
+//     img: "./images/projectsImages/inaed_logo.png",
+//     url: "https://gerenciamento-de-cursos-frontend-1muo.vercel.app/",
+//     description: `
+//     Bem-vindo ao E-commerce de Cursos, uma plataforma intuitiva e eficiente desenvolvida para facilitar a compra e gerenciamento de cursos online.
+//     Este projeto é uma aplicação web construída com Next.js, oferecendo uma experiência rápida e responsiva para os usuários. A plataforma se integra a uma API de backend para fornecer funcionalidades essenciais, como listagem de cursos, adição ao carrinho e finalização de compras.
+//     `,
+//     technologies: ["Next", "ChakraUI"],
+//     github: "https://github.com/FelipeMedeiros99/Gerenciamento-de-cursos-frontend"
+//   },
+//   {
+//     name: "Gerenciamento de cursos API",
+//     img: "./images/projectsImages/inaed_logo.png",
+//     url: "",
+//     description: `  
+//     Bem-vindo à API de gerenciamento de cursos! Esta API foi desenvolvida com NestJS e oferece endpoints para o gerenciamento de cursos, usuario e carrinho de compras
+//     `,
+//     technologies: ["Nest", "TypeScript", "PostgreSQL"],
+//     github: "https://github.com/FelipeMedeiros99/Gerenciamento-de-cursos-backend"
+//   },
+//   {
+//     name: "Cardápio Tambaú",
+//     img: "./images/projectsImages/tambau.png",
+//     url: "https://cardapio-tambau.vercel.app/",
+//     description: `
+//       O Cardápio Digital é uma solução moderna e interativa para restaurantes, bares e lanchonetes que desejam oferecer uma experiência mais prática e eficiente aos seus clientes.
+//       Desenvolvido como uma aplicação web, o sistema permite que os usuários visualizem o menu completo, incluindo descrição dos pratos, preços e imagens, diretamente de seus dispositivos. Além disso, pode contar com funcionalidades extras, como categorização de itens, personalização de pedidos e integração com sistemas de pagamento.
+//     `,
+//     technologies: ["React", "Styled-components"],
+//     github: "https://github.com/FelipeMedeiros99/cardapio-tambau"
+//   },
+//   {
+//     name: "CineFlex",
+//     img: "./images/projectsImages/cinema.png",
+//     url: "https://cine-flex-git-main-felipe-medeiros-projects-86f8f20a.vercel.app/",
+//     description: `
+//     O Sistema de Compra de Ingressos para Cinema é uma plataforma intuitiva e eficiente que permite aos usuários selecionar filmes, escolher assentos e adquirir ingressos online de forma prática e segura.
+//     Desenvolvido para oferecer uma experiência fluida e acessível, o sistema permite a navegação por cartaz de filmes, escolha de horários e pagamento integrado, garantindo comodidade tanto para os clientes quanto para a administração do cinema.
+//     `,
+//     technologies: ["React", "styled-components"],
+//     github: "https://github.com/FelipeMedeiros99/Cine-flex/tree/main"
+//   },
+//   // {
+//   //   name: "",
+//   //   img: "",
+//   //   url: "",
+//   //   description: "",
+//   //   technologies: [],
+//   //   github: ""
+//   // },
+// ];
+
+
 export const projects = [
   {
     name: "Track-it",
     img: './images/projectsImages/track-it.svg',
     url: "https://track-it-seven-amber.vercel.app/",
-    description: `
-    O Sistema de Gerenciamento de Rotina é uma ferramenta desenvolvida para auxiliar no controle e validação de tarefas diárias, garantindo maior organização, produtividade e eficiência.
-    Com essa solução, os usuários podem cadastrar, monitorar e validar suas atividades, garantindo que nenhuma tarefa essencial seja esquecida. O sistema permite a criação de fluxos de trabalho personalizados, estabelecendo regras e verificações para garantir que cada passo da rotina seja cumprido corretamente.
-    `,
+    functions: [
+      "Cadastro e login",
+      "Adicionar hábitos",
+      "Marcar hábitos como feitos",
+      "Remover hábitos",
+      "Visualizar progresso",
+      "Interface intuitiva"
+    ],
     technologies: ["react", "styled-components"],
     github: "https://github.com/FelipeMedeiros99/track-it"
   },
@@ -20,10 +113,15 @@ export const projects = [
     name: "Agenda escolar",
     img: './images/projectsImages/school-spreadsheet.png',
     url: "http://school-spreadsheet-front.s3-website-sa-east-1.amazonaws.com/sign-in",
-    description: `
-      O Sistema de Gerenciamento de Alunos é uma aplicação eficiente para armazenar e organizar informações acadêmicas, permitindo um controle preciso dos estudantes.
-      Com uma interface intuitiva, o sistema permite cadastrar, visualizar, editar e excluir alunos, garantindo que os dados estejam sempre atualizados. Todas as informações, como nome completo, idade e turma, são apresentadas de forma clara em uma tabela dinâmica, facilitando a gestão e a consulta de registros.
-    `,
+    functions: [
+      "Design responsivo",
+      "Validação de dados nos formulários",
+      "Uso do react-hook-form para otimização",
+      "Tabela de estudantes com ordenação e pesquisa",
+      "Paginação para melhor performance",
+      "Índice de pesquisa para navegação rápida",
+      "Edição e exclusão de registros"
+    ],
     technologies: ["TypeScript", "React", "chakra UI", "AWS"],
     github: "https://github.com/FelipeMedeiros99/school-spreadsheet-front"
   },
@@ -31,11 +129,16 @@ export const projects = [
     name: "Agenda escolar API",
     img: "./images/projectsImages/school-spreadsheet.png",
     url: "",
-    description: `
-    Esta API foi desenvolvida para gerenciar o cadastro de estudantes e usuários, permitindo operações essenciais como criação, edição e exclusão de alunos, garantindo controle total aos usuários autenticados.
-    A segurança é uma prioridade no projeto, com a implementação de criptografia de senhas, autenticação via tokens e validação de dados utilizando Joi, assegurando a proteção e a consistência das informações.
-    Os dados são armazenados e gerenciados de forma eficiente no banco de dados PostgreSQL, garantindo escalabilidade e confiabilidade.
-    `,
+    functions: [
+      "Cadastro de usuário com validação e criptografia de senha",
+      "Login de usuário com autenticação JWT e expiração de token",
+      "Validação de token para todas as rotas protegidas",
+      "Contagem de estudantes armazenados no banco",
+      "Busca paginada e filtrada de estudantes",
+      "Cadastro de estudante com validação e padronização de dados",
+      "Edição de estudante com atualização por ID",
+      "Remoção de estudante do banco de dados"
+    ],
     technologies: ["TypeScript", "Express", "Bcrypt", "Joi", "PostgreSQL", "Prisma"],
     github: "https://github.com/FelipeMedeiros99/school-spreadsheet-backend/tree/main"
   },
@@ -43,10 +146,12 @@ export const projects = [
     name: "Gerenciamento de Cursos",
     img: "./images/projectsImages/inaed_logo.png",
     url: "https://gerenciamento-de-cursos-frontend-1muo.vercel.app/",
-    description: `
-    Bem-vindo ao E-commerce de Cursos, uma plataforma intuitiva e eficiente desenvolvida para facilitar a compra e gerenciamento de cursos online.
-    Este projeto é uma aplicação web construída com Next.js, oferecendo uma experiência rápida e responsiva para os usuários. A plataforma se integra a uma API de backend para fornecer funcionalidades essenciais, como listagem de cursos, adição ao carrinho e finalização de compras.
-    `,
+    functions: [
+      "Login de usuário sem senha, apenas com nome salvo no banco",
+      "Visualização de cursos disponíveis e detalhes individuais",
+      "Cadastro de novos cursos com informações detalhadas",
+      "Gerenciamento do carrinho: adicionar, remover e finalizar compra"  
+    ],
     technologies: ["Next", "ChakraUI"],
     github: "https://github.com/FelipeMedeiros99/Gerenciamento-de-cursos-frontend"
   },
@@ -54,9 +159,11 @@ export const projects = [
     name: "Gerenciamento de cursos API",
     img: "./images/projectsImages/inaed_logo.png",
     url: "",
-    description: `  
-    Bem-vindo à API de gerenciamento de cursos! Esta API foi desenvolvida com NestJS e oferece endpoints para o gerenciamento de cursos, usuario e carrinho de compras
-    `,
+    functions: [
+      "Armazenamento de dados de compras",
+      "Rotas de edição, exclusão e adição de cursos",
+      "Operações de CRUD",
+    ],
     technologies: ["Nest", "TypeScript", "PostgreSQL"],
     github: "https://github.com/FelipeMedeiros99/Gerenciamento-de-cursos-backend"
   },
@@ -64,10 +171,11 @@ export const projects = [
     name: "Cardápio Tambaú",
     img: "./images/projectsImages/tambau.png",
     url: "https://cardapio-tambau.vercel.app/",
-    description: `
-      O Cardápio Digital é uma solução moderna e interativa para restaurantes, bares e lanchonetes que desejam oferecer uma experiência mais prática e eficiente aos seus clientes.
-      Desenvolvido como uma aplicação web, o sistema permite que os usuários visualizem o menu completo, incluindo descrição dos pratos, preços e imagens, diretamente de seus dispositivos. Além disso, pode contar com funcionalidades extras, como categorização de itens, personalização de pedidos e integração com sistemas de pagamento.
-    `,
+    functions: [
+      "Exibição de Categorias de Produtos",
+      "Detalhamento de Produtos",
+      "Informações de Preço e Porção"
+    ],
     technologies: ["React", "Styled-components"],
     github: "https://github.com/FelipeMedeiros99/cardapio-tambau"
   },
@@ -75,22 +183,18 @@ export const projects = [
     name: "CineFlex",
     img: "./images/projectsImages/cinema.png",
     url: "https://cine-flex-git-main-felipe-medeiros-projects-86f8f20a.vercel.app/",
-    description: `
-    O Sistema de Compra de Ingressos para Cinema é uma plataforma intuitiva e eficiente que permite aos usuários selecionar filmes, escolher assentos e adquirir ingressos online de forma prática e segura.
-    Desenvolvido para oferecer uma experiência fluida e acessível, o sistema permite a navegação por cartaz de filmes, escolha de horários e pagamento integrado, garantindo comodidade tanto para os clientes quanto para a administração do cinema.
-    `,
+    functions: [
+      "Seleção de filmes disponíveis",
+      "Escolha de sessão e horário",
+      "Seleção de assentos",
+      "Confirmação de reserva"
+    ],
     technologies: ["React", "styled-components"],
     github: "https://github.com/FelipeMedeiros99/Cine-flex/tree/main"
   },
-  // {
-  //   name: "",
-  //   img: "",
-  //   url: "",
-  //   description: "",
-  //   technologies: [],
-  //   github: ""
-  // },
 ];
+
+
 
 export const contacts = [
   {
@@ -119,17 +223,17 @@ export const hrefs = [
   {
     title: "Início",
     id: "profile",
-    icon: <FaHome /> 
+    icon: <FaHome />
   },
   {
     title: "Sobre",
     id: "about",
-    icon: <IoIosInformationCircleOutline /> 
+    icon: <IoIosInformationCircleOutline />
   },
   {
     title: "Tecnologias",
     id: "tech",
-    icon: <FaReact /> 
+    icon: <FaReact />
   },
   {
     title: "Projetos",
